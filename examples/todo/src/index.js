86942dc761c1ad4849f0133eca2c4e45a8b3fff0
Louis Pan <louis@lory.local>
2017-03-18 22:07:29 +1100

    gh-pags and deployment scripts

--- examples/todo/src/index.js
+++ examples/todo/src/index.js
@@ -3,4 +3,4 @@ import '../node_modules/todomvc-common/base.css';
 import '../node_modules/todomvc-app-css/index.css';
 
 // This has the side effect of running the haskell main after all javascript is loaded
-import {} from '../build/todo';
+import {} from '../.build/todo';
