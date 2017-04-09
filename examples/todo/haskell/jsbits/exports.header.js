9f432fb6d143871eb18fb24902d4d9aabae09e6e
Louis Pan <louis@lory.local>
2017-04-09 14:40:53 +1000

    removed need for exports

--- examples/todo/haskell/jsbits/exports.header.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-// Webpack can shim this into a NodeJS module, so 'exports' and 'require' is in scope
-// var React = require('react');
-// var ReactDOM = require('react-dom');
--- examples/todo/haskell/jsbits/exports.js
+++
@@ -1,1 +0,0 @@
-exports["mkCombinedElements"] = hgr$mkCombinedElements;
