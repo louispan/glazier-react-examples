c90a6222b0638d80c927841d04df1c4f57eb233b
Louis Pan <louis@pan.me>
2017-12-30 23:00:43 +1100

    Todo wip

--- examples/todo/haskell/app/Todo/Todo.hs
+++ examples/todo/haskell/app/Todo/Todo.hs
@@ -4,17 +4,17 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Todo.Todo where
 
-import qualified Control.Disposable as CD
 import Control.Lens
 import Data.Generics.Product
 import Data.Diverse.Profunctor
-import qualified Data.DList as D
+import Data.IORef
+import qualified Data.DList as DL
 import qualified Data.JSString as J
 import qualified GHC.Generics as G
 import qualified GHCJS.Foreign.Callback as J
@@ -22,6 +22,23 @@ import qualified GHCJS.Types as J
 import qualified Glazier.React as R
 import qualified Glazier.React.Framework as F
 import qualified JavaScript.Extras as JE
+
+data TodoEditRef = TodoEditRef J.JSVal
+
+-- onListingDeleteItem :: (R.MonadReactor x m)
+--   => (s -> m CD.Disposable)
+--   -> IORef v
+--   -> Lens' v (F.ComponentPlan x m, Listing m s s)
+--   -> ListingDeleteItem
+--   -> m (DL.DList C.Rerender)
+-- onListingDeleteItem fin ref this (ListingDeleteItem k) = do
+--        R.doModifyIORefM ref $ \obj -> do
+--             let mi = M.lookup k (obj ^. this._2.field @"items")
+--             fin' <- fromMaybe (pure mempty) (fin <$> mi)
+--             pure $ obj & (this._2.field @"items" %~ M.delete k)
+--                    . (this._1.field @"disposeOnUpdated" %~ (<> fin'))
+--                    . (this._2.field @"list" .~ []) -- this tells render to update displayItems
+--        DL.singleton <$> C.mkRerender ref (this._1) (pure mempty)
 
 -- data Command
 --     = RenderCommand (R.Gizmo Model Plan) [JE.Property] J.JSVal
@@ -49,118 +66,53 @@ data TodoInfo = TodoInfo
     , autoFocusEdit :: Bool
     } deriving G.Generic
 
-instance CD.Dispose TodoInfo
-
 data TodoPlan = TodoPlan
-    { fireToggleComplete :: Maybe (J.Callback (J.JSVal -> IO ()))
-    , fireStartEdit :: Maybe (J.Callback (J.JSVal -> IO ()))
-    , fireDestroy :: Maybe (J.Callback (J.JSVal -> IO ()))
-    , fireCancelEdit :: Maybe (J.Callback (J.JSVal -> IO ()))
+    { editRef :: J.JSVal
+    , onToggleComplete :: Maybe (J.Callback (J.JSVal -> IO ()))
+    , onStartEdit :: Maybe (J.Callback (J.JSVal -> IO ()))
+    , onDestroy :: Maybe (J.Callback (J.JSVal -> IO ()))
+    , onCancelEdit :: Maybe (J.Callback (J.JSVal -> IO ()))
     , onEditKeyDown :: Maybe (J.Callback (J.JSVal -> IO ()))
-    , onComponentRef :: Maybe (J.Callback (J.JSVal -> IO ()))
     , onEditRef :: Maybe (J.Callback (J.JSVal -> IO ()))
     } deriving G.Generic
-
-instance CD.Dispose TodoPlan
-
 
 type TodoModel = (TodoPlan, TodoInfo)
 
 
--- type Model = Schema
--- type Outline = Schema
--- instance R.ToOutline Model Outline where outline = id
+whenTodoEditRef :: (R.MonadReactor x m)
+  => IORef v
+  -> Lens' v (F.ComponentPlan x m, TodoModel)
+  -> TodoEditRef
+  -> m (DL.DList c)
+whenTodoEditRef ref this (TodoEditRef j) = do
+       R.doModifyIORef' ref (set' (this._2._1.field @"editRef") j)
+       pure mempty
 
--- mkModel :: Outline -> F (R.Maker Action) Model
--- mkModel = pure
-
--- data Plan = Plan
---     { _component :: R.ReactComponent
---     , _key :: J.JSString
---     , _frameNum :: Int
---     , _componentRef :: J.JSVal
---     , _editRef :: J.JSVal
---     , _onRender :: J.Callback (J.JSVal -> IO J.JSVal)
---     , _onComponentRef :: J.Callback (J.JSVal -> IO ())
---     , _onEditRef :: J.Callback (J.JSVal -> IO ())
---     , _onComponentDidUpdate :: J.Callback (J.JSVal -> IO ())
---     , _onEditRef :: J.Callback (J.JSVal -> IO ())
---     , _fireToggleComplete :: J.Callback (J.JSVal -> IO ())
---     , _fireStartEdit :: J.Callback (J.JSVal -> IO ())
---     , _fireDestroy :: J.Callback (J.JSVal -> IO ())
---     , _fireCancelEdit :: J.Callback (J.JSVal -> IO ())
---     , _onEditKeyDown :: J.Callback (J.JSVal -> IO ())
---     } deriving G.Generic
-
--- makeClassyPrisms ''Action
--- makeClassy ''Plan
--- makeClassy ''Schema
-
--- mkPlan :: R.Frame Model Plan -> F (R.Maker Action) Plan
--- mkPlan mm = Plan
---     <$> R.getComponent
---     <*> R.mkKey
---     <*> pure 0
---     <*> pure J.nullRef
---     <*> pure J.nullRef
---     <*> (R.mkRenderer mm $ const render)
---     <*> (R.mkHandler $ pure . pure . ComponentRefAction)
---     <*> (R.mkHandler $ pure . pure . const ComponentDidUpdateAction)
---     <*> (R.mkHandler $ pure . pure . EditRefAction)
---     <*> (R.mkHandler $ pure . pure . const ToggleCompletedAction)
---     <*> (R.mkHandler $ pure . pure . const StartEditAction)
---     <*> (R.mkHandler $ pure . pure . const DestroyAction)
---     -- <*> (R.mkHandler $ pure . pure . const CancelEditAction)
---     <*> (R.mkHandler onCancelEdit')
---     <*> (R.mkHandler onEditKeyDown')
-
--- instance CD.Disposing Plan
--- instance CD.Disposing Model where
---     disposing _ = CD.DisposeNone
-
--- -- Link Glazier.React.Model's HasPlan/HasModel with this widget's HasPlan/HasModel from makeClassy
--- instance HasPlan (R.Scene Model Plan) where
---     plan = R.plan
--- instance HasSchema (R.Scene Model Plan) where
---     schema = R.model
--- instance HasPlan (R.Gizmo Model Plan) where
---     plan = R.scene . plan
--- instance HasSchema (R.Gizmo Model Plan) where
---     schema = R.scene . schema
-
--- type Widget = R.Widget Action Outline Model Plan Command
--- widget :: Widget
--- widget = R.Widget
---     mkModel
---     mkPlan
---     window
---     gadget
-
-todoDisplay :: ( R.MonadReactor x m, HasItem' TodoModel ss) => F.Display m (F.ComponentPlan, ss) ()
+todoDisplay :: ( R.MonadReactor x m, HasItem' TodoModel ss) => F.Display m (F.ComponentPlan x m, ss) ()
 todoDisplay = F.Display $ \(cp, ss) -> do
     let (p, i) = ss ^. item' @TodoModel
     R.bh "div" []
-        [ ("className", JE.classNames [ ("completed", i ^. field @"completed")
-                                      , ("editing", i ^. field @"editing")])
+        [ ("className", JE.classNames [ ("completed", completed i)
+                                      , ("editing", editing i)])
         ] $ do
        R.bh "div" [] [ ("key", "view")
                      , ("className", "view")
                      ] $ do
             R.lf "input"
-                (JE.justSnds $ [ ("onChange", p ^. field @"fireToggleComplete")
+                (JE.justSnds $ [ ("onChange", onToggleComplete p)
                                ])
                 [ ("key", "toggle")
                 , ("className", "toggle")
                 , ("type", "checkbox")
-                , ("checked", i ^. field @"completed" . to JE.toJS')
+                , ("checked", JE.toJS' $ completed i)
                 ]
             R.bh "label"
-                (JE.justSnds $ [ ("onDoubleClick", p ^. field @"fireStartEdit")
+                (JE.justSnds $ [ ("onDoubleClick", onStartEdit p)
                                ])
                 [ ("key", "label")
-                ] (i ^. field @"value" . to R.txt)
+                ] (R.txt $ value i)
             R.lf "button"
-                (JE.justSnds $ [ ("onClick", p ^. field @"fireDestroy")
+                (JE.justSnds $ [ ("onClick", onDestroy p)
                                ])
                 [ ("key", "destroy")
                 , ("className", "destroy")
@@ -169,66 +121,19 @@ todoDisplay = F.Display $ \(cp, ss) -> do
        -- in order for react to use the new defaultValue
        R.lf "input"
            (JE.justSnds $
-               [ ("ref", p ^. field @"onEditRef")
-               , ("onBlur", p ^. field @"fireCancelEdit")
-               , ("onKeyDown", p ^. field @"onEditKeyDown")
+               [ ("ref", onEditRef p)
+               , ("onBlur", onCancelEdit p)
+               , ("onKeyDown", onEditKeyDown p)
                ])
            [ ("key", JE.toJS' $ J.unwords
-                                       [ cp ^. field @"key" . to R.runReactKey
-                                       , cp ^. field @"frameNum" . to show . to J.pack
+                                       [ R.runReactKey $ F.key cp
+                                       , J.pack . show $ F.frameNum cp
                                        ])
            , ("className", "edit")
-           , ("defaultValue", i ^. field @"value" . to JE.toJS')
-           , ("defaultChecked", i ^. field @"completed" . to JE.toJS')
+           , ("defaultValue", JE.toJS' $ value i)
+           , ("defaultChecked", JE.toJS' $ completed i)
            ]
 
-
--- | This is used by parent components to render this component
--- window :: G.WindowT (R.Scene Model Plan) R.ReactMl ()
--- window = do
---     s <- ask
---     lift $ R.lf (s ^. component . to JE.toJS')
---         [ ("key",  s ^. key . to JE.toJS')
---         , ("render", s ^. onRender . to JE.toJS')
---         , ("ref", s ^. onComponentRef . to JE.toJS')
---         , ("componentDidUpdate", s ^. onComponentDidUpdate . to JE.toJS')
---         ]
-
--- render :: G.WindowT (R.Scene Model Plan) R.ReactMl ()
--- render = do
---     s <- ask
---     lift $ R.bh "li" [ ("className", classNames [ ("completed", s ^. completed)
---                                                 , ("editing", s ^. editing)])
---                      ] $ do
---         R.bh "div" [ ("key", "view")
---                    , ("className", "view")
---                    ] $ do
---             R.lf "input" [ ("key", "toggle")
---                          , ("className", "toggle")
---                          , ("type", "checkbox")
---                          , ("checked", s ^. completed . to JE.toJS')
---                          , ("onChange", s ^. fireToggleComplete . to JE.toJS')
---                          ]
---             R.bh "label"  [ ("key", "label")
---                           , ("onDoubleClick", s ^. fireStartEdit. to JE.toJS')
---                           ] (s ^. value . to R.txt)
---             R.lf "button" [ ("key", "destroy")
---                           , ("className", "destroy")
---                           , ("onClick", s ^. fireDestroy . to JE.toJS')
---                           ]
---         -- For uncontrolled components, we need to generate a new key per render
---         -- in order for react to use the new defaultValue
---         R.lf "input" [ ("key", JE.toJS' $ J.unwords
---                                        [ s ^. key
---                                        , s ^. frameNum . to show . to J.pack
---                                        ])
---                      , ("ref", s ^.  onEditRef . to JE.toJS')
---                      , ("className", "edit")
---                      , ("defaultValue", s ^. value . to JE.toJS')
---                      , ("defaultChecked", s ^. completed . to JE.toJS')
---                      , ("onBlur", s ^. fireCancelEdit . to JE.toJS')
---                      , ("onKeyDown", s ^. onEditKeyDown . to JE.toJS')
---                      ]
 
 
 -- onCancelEdit' :: J.JSVal -> MaybeT IO [Action]
