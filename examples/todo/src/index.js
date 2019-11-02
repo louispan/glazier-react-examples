bc3a6d1f152ac61a5fb25f67111e2d4d8d891e90
Louis Pan <louis@pan.me>
2019-11-03 03:58:02 +1100

    renamed todo2 to todo

--- examples/todo/src/index.js
+++ examples/todo/src/index.js
@@ -1,6 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import '../node_modules/todomvc-common/base.css';
-import '../node_modules/todomvc-app-css/index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import hsMain from '../.shake/hsMain.js';
 
-// This has the side effect of running the haskell main after all javascript is loaded
-import {} from '../.build-ghcjs/glazier-react-todo';
+// This has the side effect of running the haskell main
+hsMain();
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
