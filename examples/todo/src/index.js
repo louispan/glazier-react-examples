15a19d3189fa1b9939986925864bf28d47fcff7e
Louis Pan <louis@pan.me>
2019-11-07 05:22:32 +1100

    todo example wip

--- examples/todo/src/index.js
+++ examples/todo/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import hsMain from '../.shake/hsMain.js';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import 'src/index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+import hsMain from '.shake/hsMain.js';
 
 // This has the side effect of running the haskell main
 hsMain();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
